b988d3a1d155220a63f466727794a97afb416b83
James Wilson <james.wilson@cap2.co.uk>
2020-01-03 19:31:42 +0000

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 import Contact from './Contact';
+import Portfolio from './Portfolio';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('header');
