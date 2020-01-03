180526661fa4c6a90b795b505d6e1004156e1b4e
James Wilson <james.wilson@cap2.co.uk>
2020-01-03 14:18:35 +0000

    Fixed failing unit tests

--- src/Contact.js
+++ src/Contact.js
@@ -14,7 +14,7 @@ function Contact(props) {
             <center><h1 className="content-subhead">If you would like to get in contact with me</h1>
             <p> The best way currently to get in touch with me, would be to pop me a message on LinkedIn </p>
             <div class="linkedIn">
-              <a href="https://www.linkedin.com/in/jpwilson/"><li><FontAwesomeIcon icon={faLinkedin} /></li> Get In Touch</a>
+              <a href="https://www.linkedin.com/in/jpwilson/"> Get In Touch</a>
             </div>
             </center>
           </div>
